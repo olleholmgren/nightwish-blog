dfe81f2d6f297d9e58801581768c36e5fc01f7c9
olleholmgren <olle.holmgren@gmail.com>
2023-06-08 11:21:00 +0000

    Deployment Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nightwish-blog.wsgi
---
+++ nw_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NwBlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nw_blog'
