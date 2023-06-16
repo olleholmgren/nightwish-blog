350495219a0166dc9cc8c41b4cdde8894f693b10
olleholmgren <olle.holmgren@gmail.com>
2023-06-16 10:18:48 +0000

    make a test page for templates and start create models

--- nw_blog/models.py
+++ nw_blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+#Create Member model
+
+# class Member:
+#     member_id =
+#     member_name = CharField
+
+
+
+#Create Post model
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=100)
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE)
