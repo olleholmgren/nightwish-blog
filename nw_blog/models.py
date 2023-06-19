a5fdd8eb1a1a09f5941c38506671684c0d5972fc
olleholmgren <olle.holmgren@gmail.com>
2023-06-19 11:50:56 +0000

    finish admin panel

--- nw_blog/models.py
+++ nw_blog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
-#Create Member model
 
+# Create Member model
 # class Member:
 #     member_id =
 #     member_name = CharField
@@ -17,6 +18,11 @@ class Post(models.Model):
     post_title = models.CharField(max_length=100, unique=True)
     post_author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=200, unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f'{self.title} | {self.author}'
 
+class Comment(models.Model):
+    comment_title = models.CharField(max_length=100, unique=False)
