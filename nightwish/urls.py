5195a093dda6d281136fc9ae700c07fb0422a0e7
olleholmgren <olle.holmgren@gmail.com>
2023-06-22 14:40:32 +0000

    install allauth and setup registration and login

--- nightwish/urls.py
+++ nightwish/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nw_blog.urls')),
+    path('', include('nw_blog.urls'), name = 'nw_blog_urls'),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
+
 ]
