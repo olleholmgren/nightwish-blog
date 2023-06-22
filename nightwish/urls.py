ca8a7739ce1ff955dc0c0b28b57c62765d9d2870
olleholmgren <olle.holmgren@gmail.com>
2023-06-22 16:15:12 +0000

    rearrange pages. add styling with bootstrap and css

--- nightwish/urls.py
+++ nightwish/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nw_blog.urls'), name = 'nw_blog_urls'),
+    path('', include('nw_blog.urls'), name='nw_blog_urls'),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
 
--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     #path('post_view/', PostDetailView.as_view(), name='post_view'),
     path('post_view/<int:pk>', PostDetailView.as_view(), name='post_view'),
     path('create_post/', CreatePostView.as_view(), name='create_post'),
-    
 ]
 
