0a9205c122f27c9ac6d8fb2d43f27cbcb26689c9
olleholmgren <olle.holmgren@gmail.com>
2023-06-26 16:05:59 +0000

    start with blog posts grid on index and change css colors

--- nw_blog/views.py
+++ nw_blog/views.py
@@ -6,7 +6,9 @@ from .models import Post
 
 class IndexView(ListView):
     model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 8
 
 class PostDetailView(DetailView):
     model = Post
