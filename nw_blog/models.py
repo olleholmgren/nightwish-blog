b2c472204d8a919a7b671bbf538e646b955bf74b
olleholmgren <olle.holmgren@gmail.com>
2023-06-26 15:13:32 +0000

    add favorite album custom model and tidy up other models

--- nw_blog/models.py
+++ nw_blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 # from django.urls import reverse
 
 STATUS = (
@@ -12,30 +11,42 @@ class Post(models.Model):
     
     post_title = models.CharField(max_length=100, unique=True)
     post_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    excerpt = models.TextField(blank=True)
     content = models.TextField()
-    slug = models.SlugField(max_length=200, unique=True)
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+    
+    class Meta:
+        ordering = ['-created_on']
     
     def __str__(self):
         return f'{self.post_title} | {self.post_author} | {self.content}'
 
-    class Meta:
-        ordering = ['-created_on']
-
+    def likes_count(self):
+        return self.likes.count()
 
 class Comment(models.Model):
 
-    post_title = models.ForeignKey(Post, on_delete=models.CASCADE)
-    post_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_title = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post_author = models.CharField(max_length=80)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-
+    approved = models.BooleanField(default=False)
+    
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
         return f'Comment by {self.post_author} on {self.post_title}'
-    
-    # def get_absolute_url(self):
-    #      return reverse('post_view', args=(str(self.id)))
+
+class PickFavoriteAlbum(models.Model):
+
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    album_title = models.TextField()
+    review = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.album_name
