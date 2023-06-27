bddb730d9742f7a9dd842785347a10f67a46613b
olleholmgren <olle.holmgren@gmail.com>
2023-06-27 15:42:58 +0000

    install crispy-forms, create form.py and add comment and like view in views.py

--- nw_blog/views.py
+++ nw_blog/views.py
@@ -1,20 +1,86 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
+from .forms import CommentForm #tre:tjugi på snutt
 
 
 
-class IndexView(ListView):
+class IndexView(generic.ListView):
+    
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 8
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'post_view.html'
-
-class CreatePostView(CreateView):
+class CreatePostView(generic.CreateView):
+    
     model = Post
     template_name = 'create_post.html'
-    fields = ['post_title', 'content']     
+    fields = ['post_title', 'content']    
+
+class PostDetailView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_view.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+    
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_view.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
+
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_view', args=[slug]))
+
+ 
