469ae55ef23c196ce783c6c24dc5eefddd9a0af5
olleholmgren <olle.holmgren@gmail.com>
2023-06-28 13:13:25 +0000

    clean up the urlpatterns and add the like path in urls.py

--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -3,8 +3,8 @@ from .views import IndexView, PostDetailView, FavouriteAlbumView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    #path('post_view/', PostDetailView.as_view(), name='post_view'),
     path('post_view/<int:pk>', PostDetailView.as_view(), name='post_view'),
     path('favourite_album/', FavouriteAlbumView.as_view(), name='favourite_album'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
 
