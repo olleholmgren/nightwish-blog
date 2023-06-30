e01e2fc13dd35f77155fea81188d5c185b738195
olleholmgren <olle.holmgren@gmail.com>
2023-06-30 09:25:13 +0000

    deployment commit

--- nightwish/settings.py
+++ nightwish/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'ORIGIN'
 
 ALLOWED_HOSTS = ['8000-olleholmgre-nightwishbl-cez8kwiaiac.ws-eu101.gitpod.io', 'nightwish-blog.herokuapp.com', 'localhost']
 
