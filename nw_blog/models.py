8be893550e4c6b66da100404154e8db40f4b3497
olleholmgren <olle.holmgren@gmail.com>
2023-10-30 15:00:21 +0000

    add missing fields in Post model in models.py. rename fields in Post and Comment models in models.py

--- nw_blog/models.py
+++ nw_blog/models.py
@@ -1,48 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 STATUS = (
-    (0, 'Draft'), 
+    (0, 'Draft'),
     (1, 'Published')
 )
 
+
 class Post(models.Model):
-    
-    post_title = models.CharField(max_length=100, unique=True)
-    member = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
-    
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
     class Meta:
         ordering = ['-created_on']
-    
+
     def __str__(self):
-        return self.post_title
+        return self.title
 
     def likes_count(self):
         return self.likes.count()
 
-    def get_absolute_url(self):
-        return reverse('post_view', args=[str(self.id)])
 
 class Comment(models.Model):
 
-    post_title = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    member = models.CharField(max_length=80)
-    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-    
+
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
-        return f'Comment {self.content} by {self.member}'
+        return f'Comment {self.body} by {self.name}'
+
 
 class FavouriteAlbum(models.Model):
 
