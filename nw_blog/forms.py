7c44a6ee88915d6dfafc05326f7fdc7f3fddf03d
olleholmgren <olle.holmgren@gmail.com>
2023-10-30 15:17:39 +0000

    change CommentForm fields -> "body" in forms.py. add admin and summernote paths in urlpatterns in urls.py

--- nw_blog/forms.py
+++ nw_blog/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('content',)
+        fields = ('body',)
 
 
 class FavouriteAlbumForm(forms.ModelForm):
--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import IndexView, PostDetailView, FavouriteAlbumView, PostLike
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('post_view/<int:pk>', PostDetailView.as_view(), name='post_view'),
     path('favourite_album/', FavouriteAlbumView.as_view(), name='favourite_album'),
     path('like/<slug:slug>', PostLike.as_view(), name='post_like')
 ]
-
