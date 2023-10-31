da3d73d98e5a1c3367801666819632fcefcd9fc3
olleholmgren <olle.holmgren@gmail.com>
2023-10-31 19:52:59 +0000

    add another missing comma, this time in summenrnote_fields in admin.py. tidy up code for pylinter to finalise editing of admin.py

--- nw_blog/admin.py
+++ nw_blog/admin.py
@@ -10,7 +10,8 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
