84691c32b735ec924d20358b4622a407286d8577
olleholmgren <olle.holmgren@gmail.com>
2023-10-31 19:32:10 +0000

    change some fields in PostAdmin and CommentAdmin classes in admin.py

--- nw_blog/admin.py
+++ nw_blog/admin.py
@@ -5,18 +5,19 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
-    list_display = ('post_title', 'status', 'created_on')
-    search_fields = ['post_title', 'content']
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content',)
+    prepopulated_fields = {'slug': ('title')}
+    summernote_fields = ('content')
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
 
-    list_display = ('member', 'content', 'post_title', 'created_on', 'approved')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ('member', 'content')
+    search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
