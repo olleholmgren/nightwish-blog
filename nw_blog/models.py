899500e0ac34d2e0dcdaca11081c1ae26c3e2f9e
olleholmgren <olle.holmgren@gmail.com>
2023-10-31 20:00:57 +0000

    add featured_image field to Post class in models.py

--- nw_blog/models.py
+++ nw_blog/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    featured_image = CloudinaryField('image', deafult='placeholder')
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
