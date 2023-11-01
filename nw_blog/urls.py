a891df84e15202d98d18fa95d330d8bbc7501307
olleholmgren <olle.holmgren@gmail.com>
2023-11-01 11:54:23 +0000

    delete admin and summernote from blog urls.py to repair mistake

--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -4,8 +4,6 @@ from .views import IndexView, PostDetailView, FavouriteAlbumView, PostLike
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_view'),
     path('favourite_album/', FavouriteAlbumView.as_view(), name='favourite_album'),
     path('like/<slug:slug>', PostLike.as_view(), name='post_like')
