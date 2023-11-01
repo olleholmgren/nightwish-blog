27ecab852c4e197500c67bfc73c5b2702d12068a
olleholmgren <olle.holmgren@gmail.com>
2023-11-01 16:37:28 +0000

    change to from . import views in urls.py

--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import IndexView, PostDetailView, FavouriteAlbumView, PostLike
+from . import views
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post_view'),
-    path('favourite_album/', FavouriteAlbumView.as_view(), name='favourite_album'),
-    path('like/<slug:slug>', PostLike.as_view(), name='post_like')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_view'),
+    path('favourite_album/', views.FavouriteAlbumView.as_view(), name='favourite_album'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
