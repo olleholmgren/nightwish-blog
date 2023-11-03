c2e82a1af2ef12fa195a80881ce7845608d89c53
olleholmgren <olle.holmgren@gmail.com>
2023-11-03 11:13:25 +0000

    change PostDetailView path to <slug:slug> in urls.py

--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('concert_memories', views.ConcertMemoriesView.as_view(), name='concert_memories'),
-    path('post_view/', views.PostDetailView.as_view(), name='post_view'),
+    path('concert_memories/', views.ConcertMemoriesView.as_view(), name='concert_memories'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_view'),
     path('favourite_album/', views.FavouriteAlbumView.as_view(), name='favourite_album'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
