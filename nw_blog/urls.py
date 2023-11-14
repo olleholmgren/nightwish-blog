e57de375c01ea477660c628096b46b4f9daef092
olleholmgren <olle.holmgren@gmail.com>
2023-11-14 11:08:27 +0000

    remove as_view in favourite album url pattern in urls.py

--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('concert_memories/', views.ConcertMemoriesView.as_view(), name='concert_memories'),
-    path('favourite_album/', views.FavouriteAlbumView.as_view(), name='favourite_album'),
+    path('favourite_album/', views.FavouriteAlbumView, name='favourite_album'),
     path('<slug:slug>/', views.PostDetailView.as_view(), name='post_view'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
