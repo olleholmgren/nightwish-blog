5a7b937ab60ed99dc6370bf26f8f09136f607dab
olleholmgren <olle.holmgren@gmail.com>
2023-11-15 10:58:52 +0000

    add comment_edit function to views.py

--- nw_blog/views.py
+++ nw_blog/views.py
@@ -100,6 +100,27 @@ def post_like(request, slug, *args, **kwargs):
     return HttpResponseRedirect(reverse('post_view', args=[slug]))
 
 
+def comment_edit(request, slug, comment_id, *args, **kwargs):
+
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = post.comments.filter(id=comment_id).first()
+
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid() and comment.name == request.user.username:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_view', args=[slug]))
+
+
 def comment_delete(request, slug, comment_id, *args, **kwargs):
 
     queryset = Post.objects.filter(status=1)
