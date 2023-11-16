9769bfe2ff7aa468a1022030b03b51b017450b07
olleholmgren <olle.holmgren@gmail.com>
2023-11-16 14:16:32 +0000

    create edit_comment.html and delete_comment.html template and make buttons working for each function in post_view.html template

--- nw_blog/urls.py
+++ nw_blog/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('album_list/', views.AlbumListView.as_view(), name='album_list'),
     path('<slug:slug>/', views.post_view, name='post_view'),
     path('like/<slug:slug>', views.post_like, name='post_like'),
-    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    # path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     # path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
-    path('delete_comment/<int:comment_id>/<slug:slug>/', views.comment_delete, name='comment_delete')
+    path('delete_comment/<int:comment_id>/<slug:slug>/', views.comment_delete, name='comment_delete'),
+    path('edit_comment/<int:comment_id>/<slug:slug>/', views.comment_edit, name='comment_edit')
 ]
