f422c240c56b9af3bdbd34ddea3df958aa505724
olleholmgren <olle.holmgren@gmail.com>
2024-02-07 11:25:09 +0000

    delete unused html pages

--- nw_blog/forms_test.py
+++ nw_blog/forms_test.py
@@ -7,10 +7,8 @@ class TestFavouriteAlbumForm(TestCase):
     This function tests if the body
     field is required.
     """
-
     def test_comment_body_is_required(self):
         form = FavouriteAlbumForm({'body': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('body', form.errors.keys())
         self.assertEqual(form.errors['body'][0], 'This field is required.')
-        
